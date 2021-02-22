62b5d0d1b788511de4c9d9668fdc409fa8eec077
Asad Saeeduddin <masaeedu@gmail.com>
2021-02-21 21:24:19 -0500

    Simplify stuff a bit, move reusable stuff out into module

--- src/Data/Bifunctor/Traverse.purs
+++ src/Data/Bifunctor/Traverse.purs
@@ -3,70 +3,32 @@ module Data.Bifunctor.Traverse where
 import Prelude
 
 import Data.Bifunctor.Invariant (class Invariant, invmap)
-import Data.Either (Either(..))
 import Data.Bifunctor.Monoidal (class Semigroupal, class Unital, combine, introduce)
+import Data.Iterated (class LabeledAssociative, class LabeledTensor, embed, point, project)
 import Data.Symbol (class IsSymbol, SProxy(..))
 import Data.Tuple (Tuple(..))
-import Data.Variant (Variant, case_, inj, on)
+import Data.Variant (Variant)
 import Data.Variant.Internal (RLProxy(..))
 import Prim.Row (class Cons, class Lacks)
-import Record (delete, get, insert)
-import Type.Proxy (Proxy(..), Proxy3(..))
+import Record (get)
 import Type.RowList as RL
-import Unsafe.Coerce (unsafeCoerce)
 
-class ComputeExtension (t :: Type -> Type -> Type) (e :: # Type -> Type) (u :: Type) | t -> e u where
-  embed :: ∀ s a r r'. IsSymbol s => Lacks s r' => Cons s a r' r => SProxy s -> t a (e r') -> e r
-  project :: ∀ s a r r'. IsSymbol s => Lacks s r' => Cons s a r' r => SProxy s -> e r -> t a (e r')
-  elim :: Proxy3 t -> u -> e ()
-  contraElim :: Proxy3 t -> e () -> u
-
-instance computeExtensionTuple :: ComputeExtension Tuple Record Unit where
-  embed s (Tuple a r') = insert s a r'
-  project s r = Tuple (get s r) (delete s r)
-  contraElim _ _ = unit
-  elim _ _ = {}
-
-instance computeExtensionEither :: ComputeExtension Either Variant Void where
-  embed s e = case e of
-    Left o -> inj s o
-    Right v -> expandCons s v
-    where
-        -- A version of `expand` that works with a Cons constraint rather than a Union constraint
-    expandCons :: ∀ s i lt gt. IsSymbol s => Cons s i lt gt => SProxy s -> Variant lt -> Variant gt
-    expandCons l v = unsafeCoerce v
-  project s v = on s Left Right v
-  contraElim _ = case_
-  elim _ = absurd
-
-class
-  ( ComputeExtension t1 et1 u1
-  , ComputeExtension t2 et2 u2
-  ) <= SequenceMonoidal t1 t2 u1 u2 et1 et2 (rl :: RL.RowList) (r :: # Type) (ri :: # Type) (ro :: # Type) p
-  | r -> p ri ro
+class SequenceMonoidal et1 et2 (rl :: RL.RowList) (r :: # Type) (ri :: # Type) (ro :: # Type) p | r -> p ri ro
   where
-  sequenceMonoidal
-    :: Proxy3 t1
-    -> Proxy3 t2
-    -> Proxy u1
-    -> Proxy u2
-    -> RLProxy rl
-    -> Record r
-    -> p (et1 ri) (et2 ro)
+  sequenceMonoidal :: RLProxy rl -> Record r -> p (et1 ri) (et2 ro)
 
 instance emptySequenceMonoidal ::
   ( Invariant p
   , Unital (->) u1 u2 Unit p
   , Semigroupal (->) t1 t2 Tuple p
-  , ComputeExtension t1 et1 u1
-  , ComputeExtension t2 et2 u2
-  ) => SequenceMonoidal t1 t2 u1 u2 et1 et2 RL.Nil r () () p
+  , LabeledTensor et1 t1 u1 (->)
+  , LabeledTensor et2 t2 u2 (->)
+  ) => SequenceMonoidal et1 et2 RL.Nil r () () p
   where
-  sequenceMonoidal pt1 pt2 _ _ _ _ = invmap (elim pt1) (contraElim pt1) (elim pt2) (contraElim pt2) (introduce unit :: p u1 u2)
+  sequenceMonoidal _ _ = invmap point.fwd point.bwd point.fwd point.bwd (introduce unit :: p u1 u2)
 
 instance stepSequenceMonoidal ::
   ( Invariant p
-  , Unital (->) u1 u2 Unit p
   , Semigroupal (->) t1 t2 Tuple p
   , IsSymbol x
   , Cons x (p i o) r' r
@@ -74,44 +36,46 @@ instance stepSequenceMonoidal ::
   , Cons x o ro' ro
   , Lacks x ro'
   , Lacks x ri'
-  , SequenceMonoidal t1 t2 u1 u2 et1 et2 rl r ri' ro' p
-  ) => SequenceMonoidal t1 t2 u1 u2 et1 et2 (RL.Cons x (p i o) rl) r ri ro p
+  , LabeledAssociative et1 t1 (->)
+  , LabeledAssociative et2 t2 (->)
+  , SequenceMonoidal et1 et2 rl r ri' ro' p
+  ) => SequenceMonoidal et1 et2 (RL.Cons x (p i o) rl) r ri ro p
   where
-  sequenceMonoidal pt1 pt2 pu1 pu2 _ r = invmap (embed k) (project k) (embed k) (project k) (combine (Tuple (get k r) rest) :: p (t1 i (et1 ri')) (t2 o (et2 ro')))
+  sequenceMonoidal _ r = invmap (embed k) (project k) (embed k) (project k) (combine (Tuple (get k r) rest) :: p (t1 i (et1 ri')) (t2 o (et2 ro')))
     where
     k :: SProxy x
     k = SProxy
     rest :: p (et1 ri') (et2 ro')
-    rest = sequenceMonoidal pt1 pt2 pu1 pu2 (RLProxy :: _ rl) r
+    rest = sequenceMonoidal (RLProxy :: _ rl) r
 
 sequenceMux
   :: ∀ r rl ri ro p
    . RL.RowToList r rl
-  => SequenceMonoidal Tuple Tuple Unit Unit Record Record rl r ri ro p
+  => SequenceMonoidal Record Record rl r ri ro p
   => Record r
   -> p (Record ri) (Record ro)
-sequenceMux = sequenceMonoidal (Proxy3 :: _ Tuple) (Proxy3 :: _ Tuple) (Proxy :: _ Unit) (Proxy :: _ Unit) (RLProxy :: _ rl)
+sequenceMux = sequenceMonoidal (RLProxy :: _ rl)
 
 sequenceDemux
   :: ∀ r rl ri ro p
    . RL.RowToList r rl
-  => SequenceMonoidal Either Either Void Void Variant Variant rl r ri ro p
+  => SequenceMonoidal Variant Variant rl r ri ro p
   => Record r
   -> p (Variant ri) (Variant ro)
-sequenceDemux = sequenceMonoidal (Proxy3 :: _ Either) (Proxy3 :: _ Either) (Proxy :: _ Void) (Proxy :: _ Void) (RLProxy :: _ rl)
+sequenceDemux = sequenceMonoidal (RLProxy :: _ rl)
 
 sequenceSwitch
   :: ∀ r rl ri ro p
    . RL.RowToList r rl
-  => SequenceMonoidal Tuple Either Unit Void Record Variant rl r ri ro p
+  => SequenceMonoidal Record Variant rl r ri ro p
   => Record r
   -> p (Record ri) (Variant ro)
-sequenceSwitch = sequenceMonoidal (Proxy3 :: _ Tuple) (Proxy3 :: _ Either) (Proxy :: _ Unit) (Proxy :: _ Void) (RLProxy :: _ rl)
+sequenceSwitch = sequenceMonoidal (RLProxy :: _ rl)
 
 sequenceSplice
   :: ∀ r rl ri ro p
    . RL.RowToList r rl
-  => SequenceMonoidal Either Tuple Void Unit Variant Record rl r ri ro p
+  => SequenceMonoidal Variant Record rl r ri ro p
   => Record r
   -> p (Variant ri) (Record ro)
-sequenceSplice = sequenceMonoidal (Proxy3 :: _ Either) (Proxy3 :: _ Tuple) (Proxy :: _ Void) (Proxy :: _ Unit) (RLProxy :: _ rl)
+sequenceSplice = sequenceMonoidal (RLProxy :: _ rl)
---
+++ src/Data/Iterated.purs
@@ -0,0 +1,78 @@
+module Data.Iterated where
+
+import Prelude
+
+import Control.Category.Tensor (class Associative, class Tensor, Iso)
+import Data.Either (Either(..), either)
+import Data.Profunctor.Strong ((&&&))
+import Data.Symbol (class IsSymbol, SProxy)
+import Data.Tuple (Tuple, uncurry)
+import Data.Variant (Variant, case_, inj, on)
+import Prim.Row (class Cons, class Lacks)
+import Record (delete, get, insert)
+import Unsafe.Coerce (unsafeCoerce)
+
+class Associative t p <= LabeledAssociative (e :: # Type -> Type) t p | e -> t
+  where
+  nest :: ∀ k a r r'. IsSymbol k => Lacks k r' => Cons k a r' r => SProxy k -> Iso p (t a (e r')) (e r)
+
+embed :: ∀ e t k a r' r.
+  LabeledAssociative e t (->) =>
+  IsSymbol k =>
+  Lacks k r' =>
+  Cons k a r' r =>
+  SProxy k -> t a (e r') -> e r
+embed k = (nest k).fwd
+
+project :: ∀ e t k a r' r.
+  LabeledAssociative e t (->) =>
+  IsSymbol k =>
+  Lacks k r' =>
+  Cons k a r' r =>
+  SProxy k -> e r -> t a (e r')
+project k = (nest k).bwd
+
+instance iteratedRecord :: LabeledAssociative Record Tuple (->)
+  where
+  nest k = { fwd, bwd }
+    where
+    fwd = uncurry (insert k)
+    bwd = get k &&& delete k
+
+expandCons :: ∀ k v lt gt. IsSymbol k => Cons k v lt gt => SProxy k -> Variant lt -> Variant gt
+expandCons _ = unsafeCoerce
+
+instance iteratedVariant :: LabeledAssociative Variant Either (->)
+  where
+  nest k = { fwd, bwd }
+    where
+    fwd = either (inj k) (expandCons k)
+    bwd = on k Left Right
+
+class (LabeledAssociative e t p, Tensor t i p) <= LabeledTensor e t i p | e -> t i, t -> i, i -> t
+  where
+  point :: Iso p i (e ())
+
+elim :: ∀ e t i.
+  LabeledTensor e t i (->) =>
+  i -> e ()
+elim = point.fwd
+
+contraElim :: ∀ e t i.
+  LabeledTensor e t i (->) =>
+  e () -> i
+contraElim = point.bwd
+
+instance pointedRecord :: LabeledTensor Record Tuple Unit (->)
+  where
+  point = { fwd, bwd }
+    where
+    fwd = mempty
+    bwd = mempty
+
+instance pointedVariant :: LabeledTensor Variant Either Void (->)
+  where
+  point = { fwd, bwd }
+    where
+    fwd = absurd
+    bwd = case_
