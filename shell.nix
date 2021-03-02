aa2d4d3b72f309e3f1b75e5fc51905d65a03d5e8
Asad Saeeduddin <masaeedu@gmail.com>
2021-03-01 20:45:06 -0500

    Update nix stuff

--- shell.nix
+++ shell.nix
@@ -1,12 +1,15 @@
 let
   sources = import ./nix/sources.nix;
-  pkgs = import sources.unstable {};
+  pkgs = import sources.nixpkgs-unstable {};
+  epn = import sources.easy-purescript-nix { inherit pkgs; };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [
+  buildInputs = with pkgs; with epn; [
     purescript
-    nodejs
     spago
+    pulp
+    nodejs
+    nodePackages.bower
     nodePackages.purescript-language-server
   ];
 }
